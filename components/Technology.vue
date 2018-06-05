22fc5fc8dba5a7a9efcd6f698eab4b5df919dfa2
artem <artem@coderiver.com.ua>
2018-06-05 18:08:22 +0300

    add search posts

--- components/Technology.vue
+++ components/Technology.vue
@@ -57,21 +57,13 @@ export default {
         {
           title: 'Our favourite task management app!',
           date: 'March 28, 2013',
-          url: '/grunt.png',
-          btn: 'true',
-          descr1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          descr2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        },
-        {
-          title: 'Our favourite task management app!',
-          date: 'March 28, 2013',
           url: '/macbook.png',
           btn: 'true',
           descr1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           descr2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         {
-          title: 'Our favourite task management app!',
+          title: 'iMac and MacBook',
           date: 'March 28, 2013',
           url: '/comp.png',
           btn: 'true',
@@ -79,7 +71,7 @@ export default {
           descr2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         {
-          title: 'Our favourite task management app!',
+          title: 'Grunt',
           date: 'March 28, 2013',
           url: '/grunt.png',
           btn: 'true',
@@ -87,17 +79,25 @@ export default {
           descr2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         {
-          title: 'Our favourite task management app!',
+          title: 'Vue.js',
           date: 'March 28, 2013',
-          url: '/macbook.png',
+          url: '/vue.png',
           btn: 'true',
           descr1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           descr2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         },
         {
-          title: 'Our favourite task management app!',
+          title: 'React.js',
           date: 'March 28, 2013',
-          url: '/comp.png',
+          url: '/react.png',
+          btn: 'true',
+          descr1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          descr2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+          title: 'Angular.js',
+          date: 'March 28, 2013',
+          url: '/angular.png',
           btn: 'true',
           descr1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           descr2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
